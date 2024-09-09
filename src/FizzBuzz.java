ef6c53b7390f0f6c39d3fd83d47c025607ec39ae
NKwyk <wyk5204@163.com>
2024-09-09 18:02:15 -0400

    hello

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,31 +4,62 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+        int limit = 100;
+        int counter = 0;
+        while (counter < limit) {
+            counter = getCounter(counter);
         }
+//        for (int i = 0; i < 100; i = i + 15) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+//            }
+        }
+
+    private static int getCounter(int counter) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = counter % 3 == 0;
+        boolean divisibleBy5 = counter % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(counter);
+
+        }
+        counter += 5;
+        return counter;
     }
 }
+
